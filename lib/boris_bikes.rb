fc04780262d8cd088b7bf15926f201bec29c5676
Ben <ben@benzeng.co.uk>
2019-08-21 14:42:26 +0100

    Ex 8 complete

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,6 +1,8 @@
 
 class DockingStation
 
+  def release_bike
+    
+  end
 
 end
-  
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/boris_bikes"
 
-describe "DockingStation" do
-
-
+describe DockingStation do
+  it "responded to release_bike method" do
+    expect(DockingStation.new).to respond_to(:release_bike)
   end
+end
