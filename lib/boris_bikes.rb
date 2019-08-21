ee3f1c842fc3963b3421d366818460cb45b2976b
Ben <ben@benzeng.co.uk>
2019-08-21 15:39:25 +0100

    Ex10 complete

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -2,10 +2,12 @@
 class DockingStation
 
   def release_bike
+    Bike.new
   end
 end
 
 class Bike
   def working?
+    return true
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,4 +4,15 @@ describe DockingStation do
   it "responded to release_bike method" do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
+
+  it "Docking Station responds to working?" do
+    expect((DockingStation.new).release_bike).to respond_to(:working?)
+  end
+
+  it "Docking Station gets a working bike" do
+    expect((DockingStation.new).release_bike.working?).to eq(true)
+
+  end
+
+
 end
