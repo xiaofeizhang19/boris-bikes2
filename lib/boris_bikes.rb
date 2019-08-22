28e9e95201f855d2fc3ef7ca25b10e85d3da4827
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-08-22 15:51:47 +0100

    ex15&16 add capacity as constant

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,19 +1,29 @@
 require_relative '../lib/bike'
 
 class DockingStation
-  attr_reader :bikes
-
+  #attr_reader :bikes
+  DEFAULT_CAPACITY = 20
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock_bike(live_bike)
-    fail 'Docking station full' if @bikes.length >= 20
+    fail 'Docking station full' if full?
     @bikes << live_bike
+  end
+
+  private
+
+  def full?
+    @bikes.length >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -20,7 +20,9 @@ describe DockingStation do
 
   describe '#dock_bike' do
     it 'raises an error when full' do
-      20.times {subject.dock_bike Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times do
+        subject.dock_bike Bike.new
+      end
       expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
     end
   end
